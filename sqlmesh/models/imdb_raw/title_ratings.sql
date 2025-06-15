a7cf2228b8c99c1f3af7feeaa1972e1cf7f5342f
dnbnero <dnbneroph@gmail.com>
2025-06-16 01:01:23 +0300

    Chenged sqlmesh model to use duckdb

--- sqlmesh/models/imdb_raw/title_ratings.sql
+++ sqlmesh/models/imdb_raw/title_ratings.sql
@@ -6,8 +6,14 @@ MODEL (
   )
 );
 
+-- SELECT
+--   substr(tconst, 3)::UInt64 AS title_id,
+--   averageRating::Decimal(3, 1) AS avg_rating,
+--   numVotes::UInt64 AS num_votes
+-- FROM url('https://datasets.imdbws.com/title.ratings.tsv.gz', 'TSVWithNames')
+
 SELECT
   substr(tconst, 3)::UInt64 AS title_id,
   averageRating::Decimal(3, 1) AS avg_rating,
   numVotes::UInt64 AS num_votes
-FROM url('https://datasets.imdbws.com/title.ratings.tsv.gz', 'TSVWithNames')
+FROM 'https://datasets.imdbws.com/title.ratings.tsv.gz'
