638eb70977d4b4da0447cd8c9c77383659aac460
dnbnero <dnbneroph@gmail.com>
2025-06-16 16:18:29 +0300

    Some changes

---
+++ python/utils.py
@@ -0,0 +1,19 @@
+import httpx
+import lxml.etree as et
+import pendulum
+
+def get_history(link, date: str):
+    data = httpx.get(link).content
+
+    parser = et.XMLPullParser(tag='dataversion')
+    parser.feed(data)
+
+    return [
+        {
+            'source': ver.findtext('source'),
+            'created': ver.findtext('created')
+        }
+        for _,ver
+        in parser.read_events()
+        if pendulum.parse(ver.findtext('created')).format('YYYY-MM-DD') == date
+    ]
